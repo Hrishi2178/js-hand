34f1f83d15897d2e5097850751110ae0a08a3cfd
Hrishikesh Karale <hrishikeshkarale06@gmail.com>
2025-12-08 03:51:15 +0530

    datatypes

---
+++ 01_basics.js
@@ -0,0 +1,20 @@
+const accountId=12342
+let accountEmail="karale@gmail.com"
+var accountPassword="ab1234cd"
+accountCity="pune"
+
+accountEmail="karale@rediffmail.com"
+accountPassword="xyz234zyx"
+accountCity="delhi"
+accountState;
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity,accountState])
+/*
+table use kia toh wapas waps likhane ki jhanjat nahi
+
+var ko kabhi bhi use nahi karna hai kyu ke function scope ya block scope me dikkat aati hai
+
+agar kisi ko define nmahi kia to uski valur undefined rahegi
+
+
+*/
---
+++ 02_datatype.js
@@ -0,0 +1,19 @@
+"use strict";
+//treat all js code as per newer version
+//alert(3+3) //this will not work in strict mode if not run in browser
+//semicolon is mandatory in strict mode
+//code redability rahli pahije
+let name="hrishikesh"
+let age=19
+let isLoggedIn=false
+
+//number=>
+    //nihInt
+//string
+//boolean=>true/false
+//null=>standalone
+//undefined
+//symbol(unique identity)
+//bigint(large number)
+//object=>key value pair
+console.log(typeof "hrishikesh", typeof age)
