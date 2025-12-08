dbe8bc84674fcaf956954d54a2503f10f84a8d6e
Hrishikesh Karale <hrishikeshkarale06@gmail.com>
2025-12-08 11:04:02 +0530

    this is 3rd repo

---
+++ 03_conversionoperation.js
@@ -0,0 +1,23 @@
+let score="33ab"
+console.log(typeof score);
+console.log(typeof(score));
+let valueinNumber=Number(score)
+console.log(typeof valueinNumber);
+console.log(valueinNumber);
+//NaN=>not a number
+//"33"ko convert karte hai toh voh easily number me convert ho jata hai
+//"33ab" ko convert karte hai toh voh NaN return karta hai
+//true=>1
+//false=>0
+let isLoggedIn="hrvk"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//type conversion
+//string to number
+//""=>0
+//"hrvk"=>true
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+//number to string
